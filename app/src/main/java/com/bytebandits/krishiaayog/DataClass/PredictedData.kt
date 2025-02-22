096e7e8c328d0012013a4b6d16a64e98194b29e5
Ambarish Manna <amanna1041@gmail.com>
2025-02-22 22:43:38 +0530

    Delete app directory

--- app/src/main/java/com/bytebandits/krishiaayog/DataClass/PredictedData.kt
+++
@@ -1,9 +0,0 @@
-package com.bytebandits.krishiaayog.DataClass
-
-data class PredictedData (
-    val cause: String,
-    val confidence: Double,
-    val cure: String,
-    val description: String,
-    val predicted_class: String
-)
